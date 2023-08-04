05344cdde8a6d8b118773b21b3455ac4ce4e5fee
Eric Ryan Harrison <me@ericharrison.info>
2023-08-04 01:48:01 -0400

    Change search paths

--- scripts/tmux-sessionizer.sh
+++ scripts/tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/dotfiles ~/development ~/development/makestartups ~/development/explorations -mindepth 1 -maxdepth 1 -type d -not -path '*/.*'  | fzf)
+    selected=$(find ~/dotfiles ~/development/makestartups ~/development/projects ~/development/explorations ~/development/rsc ~/development -mindepth 1 -maxdepth 1 -type d -not -path '*/.*'  | fzf)
 fi
 
 if [[ -z $selected ]]; then
